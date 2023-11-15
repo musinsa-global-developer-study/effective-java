671ce4162e718a8e895bb8ec65c7adf1a407c44c
하수종 <hsz3855@gmail.com>
2023-11-15 22:55:15 +0900

    add : item-83 순환 참조 예시 코드를 추가한다

---
+++ example/src/chapter11/item83/ClassA.java
@@ -0,0 +1,14 @@
+package chapter11.item83;
+
+public class ClassA {
+
+    private final ClassB classB;
+
+    public ClassA(ClassB classB) {
+        this.classB = classB;
+    }
+
+    public ClassB getClassB() {
+        return classB;
+    }
+}
